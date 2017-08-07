82feef48e96cd83dde9279c873c185a6266f5dc5
lossp <richard.xxmxx@gmail.com>
2017-08-07 15:29:37 +0800

    nodejs

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var axios = require('axios');
+var data = {};
+
+if(process.argv[2]){
+  data.params = {
+    city: process.argv[2]
+  }
+}
+
+axios.get('http://api.jirengu.com/weather.php', data)
+  .then(function (response) {
+      var weather = response.data.results[0].weather_data[0];
+      var city = response.data.results[0].currentCity;
+      console.log(city);
+      console.log(weather.date);
+      console.log(weather.temperature)
+      console.log(weather.weather + ',' + weather.wind)
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
